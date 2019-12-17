46a7f60cf5b5133db93a59158e5392b3d8fde944
Julie Cantin <julie.cantin@nventive.com>
2019-12-17 15:24:52 -0500

    add test

---
+++ TextBlockMultiline/TextBlockMultiline.Shared/MyViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBlockMultiline
+{
+	public class MyViewModel
+	{
+		public string Message => "1st line of 5-line message \r\n 2nd line \r\n 3rd line \r\n 4th line \r\n 5th line.";
+	}
+}
